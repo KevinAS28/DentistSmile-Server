5e8d07f2617efbb3006e51575bd9c1522a7b38c3
Anggraini Dwiansyah <anggraini.d@mail.ugm.ac.id>
2022-04-14 08:29:19 +0700

    database seeder admin dokter ortu dll

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Dokter;
+use App\Models\Orangtua;
+use App\Models\Kecamatan;
+use App\Models\Kelurahan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,23 +23,38 @@ class DatabaseSeeder extends Seeder
             'password'  =>  bcrypt('admin1234'),
             
         ]);
-        // $dokter = User::create([
-        //     'email'     => 'dokter@dokter.com',
-        //     'password'  =>  bcrypt('dokter1234'),
+        $dokter = User::create([
+            'email'     => 'dokter@dokter.com',
+            'password'  =>  bcrypt('dokter1234'),
+            'role'      => 'dokter',
             
-        // ]);
-        // $dokter2 = User::create([
-        //     'email'     => 'dokter2@dokter.com',
-        //     'password'  =>  bcrypt('dokter1234'),
-        //     'role'      => 'dokter',
+        ]);
+        $ortu = User::create([
+            'email'     => 'ortu@ortu.com',
+            'password'  =>  bcrypt('ortu1234'),
+            'role'      => 'orangtua',
             
-        // ]);
-        // $dokter3 = User::create([
-        //     'email'     => 'dokter3@dokter.com',
-        //     'password'  =>  bcrypt('dokter1234'),
-        //     'role'      => 'dokter',
+        ]);
+        $kecamatan = Kecamatan::create([
+            'nama'     => 'Tambun',
             
-        // ]);
+        ]);
+        $kelurahan = Kelurahan::create([
+            'nama'     => 'Mekarsari',
+            'id_kecamatan' => 1,
+            
+        ]);
+        $dokter = Dokter::create([
+            'id_users'  => 2,
+            'nik'       => '123456',
+            'nama' => 'Indira',
+            'jenis_kelamin' => 'Perempuan',
+            'tempat_lahir'  => 'Bekasi',
+            'tanggal_lahir' => '2000-01-01',
+            'no_telp'   => '088210968646',
+            'no_str'    => '654321',
+            
+        ]);
 
 
     }
