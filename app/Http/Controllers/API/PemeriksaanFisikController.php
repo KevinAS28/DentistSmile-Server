8dd3613fd430e153e79cd56d6db2a92516d7dc78
rizalmifta79 <rizalmifta79@gmail.com>
2022-11-18 13:23:16 +0700

    revisi response pemeriksaan telinga

--- app/Http/Controllers/API/PemeriksaanFisikController.php
+++ app/Http/Controllers/API/PemeriksaanFisikController.php
@@ -47,73 +47,73 @@ class PemeriksaanFisikController extends Controller
         DB::beginTransaction();
         try{
 
-        $user = Auth::user();
-        $orangtua = Orangtua::Where('id_users', Auth::user()->id)->value('id');
-        $anak = Anak::Where('id_orangtua',$orangtua)->get();
+            $user = Auth::user();
+            $orangtua = Orangtua::Where('id_users', Auth::user()->id)->value('id');
+            $anak = Anak::Where('id_orangtua',$orangtua)->get();
 
-        $waktu_pemeriksaan = Carbon::now(); //mendapatkan waktu sekarang
+            $waktu_pemeriksaan = Carbon::now(); //mendapatkan waktu sekarang
 
-        $pFisik = new PemeriksaanFisik();
-        $pFisik->id_anak =  $request->id_anak;
-        $pFisik->id_sekolah= $request->id_sekolah;
-        $pFisik->id_kelas = $request->id_kelas;
-        $pFisik->tinggi_badan = $request->tinggi_badan;
-        $pFisik->berat_badan = $request->berat_badan;
-        if($request->tinggi_badan > 0 && $request->berat_badan > 0){
-            $pFisik->imt = $request->berat_badan/((($request->tinggi_badan)/100)*(($request->tinggi_badan)/100)); // perhitungan IMT
+            $pFisik = new PemeriksaanFisik();
+            $pFisik->id_anak =  $request->id_anak;
+            $pFisik->id_sekolah= $request->id_sekolah;
+            $pFisik->id_kelas = $request->id_kelas;
+            $pFisik->tinggi_badan = $request->tinggi_badan;
+            $pFisik->berat_badan = $request->berat_badan;
+            if($request->tinggi_badan > 0 && $request->berat_badan > 0){
+                $pFisik->imt = $request->berat_badan/((($request->tinggi_badan)/100)*(($request->tinggi_badan)/100)); // perhitungan IMT
+            }
+            $pFisik->sistole = $request->sistole;
+            $pFisik->diastole = $request->diastole;
+            $pFisik->waktu_pemeriksaan = $waktu_pemeriksaan;
+            $pFisik->save(); // MENYIMPAN PEMERIKSAAN FISIK
+
+            $pMata = new PemeriksaanMata();
+            $pMata->id_anak =  $request->id_anak;
+            $pMata->id_sekolah= $request->id_sekolah;
+            $pMata->id_kelas = $request->id_kelas;
+            $pMata->msoal1=$request->msoal1; // menyimpan jawaban soal mata
+            $pMata->msoal2=$request->msoal2; // menyimpan jawaban soal mata
+            $pMata->msoal3=$request->msoal3; // menyimpan jawaban soal mata
+            $pMata->msoal4=$request->msoal4; // menyimpan jawaban soal mata
+            $pMata->msoal5=$request->msoal5; // menyimpan jawaban soal mata
+            $pMata->msoal6=$request->msoal6; // menyimpan jawaban soal mata (menentukan keterangan mata minus plus atau butawarna)
+            $pMata->msoal7=$request->msoal7; // menyimpan jawaban soal mata
+            $pMata->waktu_pemeriksaan=$waktu_pemeriksaan;
+            $pMata->save(); // MENYIMPAN PEMERIKSAAN MATA
+            $pTelinga = new PemeriksaanTelinga();
+            $pTelinga->id_anak =  $request->id_anak;
+            $pTelinga->id_sekolah= $request->id_sekolah;
+            $pTelinga->id_kelas = $request->id_kelas;
+            $pTelinga->tsoal1=$request->tsoal1;
+            $pTelinga->tsoal2=$request->tsoal2;
+            $pTelinga->tsoal3=$request->tsoal3;
+            $pTelinga->tsoal4=$request->tsoal4;
+            $pTelinga->tsoal5=$request->tsoal5;
+            $pTelinga->tsoal6=$request->tsoal6;
+            $pTelinga->tsoal7=$request->tsoal7;
+            $pTelinga->tsoal8=$request->tsoal8;
+            $pTelinga->tsoal9=$request->tsoal9;
+            $pTelinga->waktu_pemeriksaan=$waktu_pemeriksaan;
+            $pTelinga->save(); // MENYIMPAN PEMERIKSAAN TELINGA
+
+
+
+            DB::commit();
+
+            return response()->json([
+                'messages'=>'success',
+                'pemeriksaan fisik'=>$pFisik,
+                'pemeriksaan mata'=>$pMata,
+                'pemerikssan telinga' =>$pTelinga
+
+            ]);
+        }catch(\Exception $e){
+            DB::rollback();
+
+            return response()->json([
+                'messages'=>'fail'
+            ]);
         }
-        $pFisik->sistole = $request->sistole;
-        $pFisik->diastole = $request->diastole;
-        $pFisik->waktu_pemeriksaan = $waktu_pemeriksaan;
-        $pFisik->save(); // MENYIMPAN PEMERIKSAAN FISIK
-        
-        $pMata = new PemeriksaanMata();
-        $pMata->id_anak =  $request->id_anak;
-        $pMata->id_sekolah= $request->id_sekolah;
-        $pMata->id_kelas = $request->id_kelas;
-        $pMata->msoal1=$request->msoal1; // menyimpan jawaban soal mata
-        $pMata->msoal2=$request->msoal2; // menyimpan jawaban soal mata
-        $pMata->msoal3=$request->msoal3; // menyimpan jawaban soal mata
-        $pMata->msoal4=$request->msoal4; // menyimpan jawaban soal mata
-        $pMata->msoal5=$request->msoal5; // menyimpan jawaban soal mata
-        $pMata->msoal6=$request->msoal6; // menyimpan jawaban soal mata (menentukan keterangan mata minus plus atau butawarna)
-        $pMata->msoal7=$request->msoal7; // menyimpan jawaban soal mata
-        $pMata->waktu_pemeriksaan=$waktu_pemeriksaan;
-        $pMata->save(); // MENYIMPAN PEMERIKSAAN MATA
-        $pTelinga = new PemeriksaanTelinga();
-        $pTelinga->id_anak =  $request->id_anak;
-        $pTelinga->id_sekolah= $request->id_sekolah;
-        $pTelinga->id_kelas = $request->id_kelas;
-        $pTelinga->tsoal1=$request->tsoal1;
-        $pTelinga->tsoal2=$request->tsoal2;
-        $pTelinga->tsoal3=$request->tsoal3;
-        $pTelinga->tsoal4=$request->tsoal4;
-        $pTelinga->tsoal5=$request->tsoal5;
-        $pTelinga->tsoal6=$request->tsoal6;
-        $pTelinga->tsoal7=$request->tsoal7;
-        $pTelinga->tsoal8=$request->tsoal8;
-        $pTelinga->tsoal9=$request->tsoal9;
-        $pTelinga->waktu_pemeriksaan=$waktu_pemeriksaan;
-        $pTelinga->save(); // MENYIMPAN PEMERIKSAAN TELINGA
-
-        
-       
-        DB::commit();
-
-        return response()->json([
-            'messages'=>'success',
-            'pemeriksaan fisik'=>$pFisik,
-            'pemeriksaan mata'=>$pMata,
-            'pemerikssan telinga' =>$pTelinga
-
-        ]);
-    }catch(\Exception $e){
-        DB::rollback();
-        
-        return response()->json([
-            'messages'=>'fail'
-        ]);
-    }
 
     }
     /**
@@ -165,29 +165,31 @@ class PemeriksaanFisikController extends Controller
         // MENAMPILKAN DATA PEMERIKSAAN FISIK
         $user = Auth::user();
         $orangtua = Orangtua::Where('id_users', Auth::user()->id)->value('id');  // MENDAPATKAN ID ORANGTUA
-        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA 
+        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA
         $pemeriksaanFisik = PemeriksaanFisik::Where('id_anak',$id)->get();
+
+
 
         // MENDAPATKAN LIST PEMERIKSAAN FISIK BERDASARKAN ID ANAK
         return response()->json([
             'messages'=> 'Success',
             'data'=> $pemeriksaanFisik,
-            
+
         ]);
-        
+
     }
 
     public function riwayatmata($id){
         // MENAMPILKAN DATA PEMERIKSAAN FISIK
         $user = Auth::user();
         $orangtua = Orangtua::Where('id_users', Auth::user()->id)->value('id');  // MENDAPATKAN ID ORANGTUA
-        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA 
+        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA
         $pemeriksaanMata = PemeriksaanMata::Where('id_anak',$id)->get();
         // MENDAPATKAN LIST PEMERIKSAAN FISIK BERDASARKAN ID ANAK
         return response()->json([
             'messages'=> 'Success',
             'data'=> $pemeriksaanMata,
-            
+
         ]);
     }
 
@@ -195,15 +197,45 @@ class PemeriksaanFisikController extends Controller
         // MENAMPILKAN DATA PEMERIKSAAN FISIK
         $user = Auth::user();
         $orangtua = Orangtua::Where('id_users', Auth::user()->id)->value('id');  // MENDAPATKAN ID ORANGTUA
-        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA 
+        $anak = Anak::Where('id_orangtua',$orangtua)->get(); // MENDAPATKAN LIST ANAK BERDAS;ARKAN ID ORANGTUA
         $pemeriksaanTelinga = PemeriksaanTelinga::Where('id_anak',$id)->get();
+
         // MENDAPATKAN LIST PEMERIKSAAN FISIK BERDASARKAN ID ANAK
+
+        $data=[];
+        foreach ($pemeriksaanTelinga as $key => $value){
+
+            if($value->tsoal7=='ya'&& $value->tsoal8=='ya'){
+                $hasil = 'Serumen 2';
+            }else if($value->tsoal7=='ya'&& $value->tsoal8=='tidak'){
+                $hasil = 'Serumen Kanan';
+            }else if($value->tsoal7=='tidak'&& $value->tsoal8=='ya'){
+                $hasil = 'Serumen Kiri';
+            }else{
+                $hasil = 'Serumen Tidak Ada';
+            }
+
+            if($value->created_at){
+                $waktu_pemeriksaan = date('d-m-Y', strtotime($value->created_at));
+            }
+
+            $data [] = [
+                'id'=> $value->id,
+                'id_anak'=> $value->id_anak,
+                'hasil'=> $hasil,
+                'waktu_pemeriksaan'=> $waktu_pemeriksaan
+            ];
+
+        }
         return response()->json([
             'messages'=> 'Success',
-            'data'=> $pemeriksaanTelinga, 
+            'data' => $data
+
+
         ]);
     }
 
 
 
 }
+
